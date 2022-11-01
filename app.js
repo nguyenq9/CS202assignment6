036640ecd56e1d1365321243183b443171253bde
nguyenq9 <77131873+nguyenq9@users.noreply.github.com>
2022-11-01 12:00:24 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+// function setup() { 
+//   loadJSON("http://api.open-notify.org/astros.json", dataReceived);
+// }
+
+
+var number;
+var names = [];
+var craft = [];
+
+// function dataReceived(data) {
+//   number = data.number
+//   console.log("number" + number)
+//   for (let i = 0; i < data.people.length; i++) {
+//     names[i] = data.people[i].name
+//     craft[i] = data.people[i].craft
+//   }
+//   console.log(names)
+//   console.log(craft)
+// }
+
+$(document).ready(function(){
+  $.getJSON("http://api.open-notify.org/astros.json", function( data ) {
+    number = data['number'];
+    for (let i = 0; i < data.people.length; i++) {
+      names[i] = data['people'][i]['name']
+      craft[i] = data['people'][i]['craft']
+    }
+  });
+});
+
+function createAstronaut(i) {
+  var x = document.createElement("IMG");
+  x.setAttribute("src", "astro.png");
+  x.setAttribute("width", "304");
+  x.setAttribute("height", "228");
+  x.setAttribute("alt", "Astronaut");
+  x.setAttribute("onclick", "teehee("+i+")");
+  if (i%2 == 1) {
+    x.setAttribute("id", "clockwise");
+  } else {
+    x.setAttribute("id", "counterclockwise");
+  }
+  document.body.appendChild(x);
+}
+
+function hehe() {
+  var elem = document.getElementById('btn');
+  elem.parentNode.removeChild(elem);
+  for (let i = 0; i < number; i++) {
+    createAstronaut(i)
+  }
+}
+
+function teehee(i) {
+  alert("Name: " + names[i] + "\nCraft: " + craft[i] );
+}
